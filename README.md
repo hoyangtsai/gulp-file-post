3b3f1b84c027ccde2548d3c0c9d197d881bdecc4
Ho-Yang Tsai <hoyang.t@gmail.com>
2016-06-18 17:33:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# gulp-file-post
+
+> Upload file to remote server via http post.
+
+## Install
+
+```
+npm install --save-dev gulp-file-post
+```
+
+## Example
+
+```js
+var gulp = require('gulp');
+var upload = require('gulp-file-post');
+
+gulp.task('upload', function() {
+  return gulp.src('./publish.zip')
+    .pipe(upload({
+      url: 'http://wapstatic.kf0309.3g.qq.com/receiver/receiver2.php',
+      destDir: '/data/wapstatic/keithytsai/open_zc'
+    })
+  );
+});
+
+```
+
+### options
+
+name | type | description
+--- | --- | --- 
+url | string | remote server receiver
+destDir | string | upload file destination
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,76 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var green = gutil.colors.green;
+var red = gutil.colors.red;
+
+var request = require('request');
+var fs = require('fs');
+var path = require('path');
+
+const PLUGIN_NAME = 'gulp-file-post';
+
+module.exports = function(options) {
+  if (options.constructor !== Object) {
+    throw new PluginError(PLUGIN_NAME, 'Parameter not a object.');
+  }
+  if (!options.hasOwnProperty('url') || !options.hasOwnProperty('destDir')) {
+    throw new PluginError(PLUGIN_NAME, 'Missing field.');
+  }
+
+  return through.obj(function(file, enc, cb) {
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+      return;
+    }
+    if (file.isNull() || file.isDirectory()) {
+      cb(null);
+    }
+
+    if (file.isBuffer()) {
+      var filePath = file.path;
+      var fileExt = path.extname(filePath).substring(1);
+
+      if (fileExt === 'zip') {
+        var formData = {
+          to: options.destDir,
+          type: fileExt,
+          file: fs.createReadStream(filePath)
+        };
+        for (var key in options) {
+          if (key == 'url' || key == 'destDir') continue;
+          formData[key] = options[key];
+        }
+        request.post({url: options.url, formData: formData}, function (err, resp, body) {
+          if (err) {
+            throw new PluginError(PLUGIN_NAME, 'Upload failed: ', err);
+          }
+          gutil.log(green('Upload successed!'));
+        });
+      } else {
+        var dirname = path.resolve(__dirname, '../..', 'publish');
+
+        var regexp = new RegExp('[\\s\\S]*' + dirname + '[\\/]?');
+        var relPath = path.dirname(filePath.replace(/\\+/g, '\/')).replace(regexp, '') + '/';
+
+        var destPath = options.destDir + '/' + relPath;
+
+        var formData = {
+          to: destPath,
+          file: fs.createReadStream(filePath)
+        };
+        for (var key in options) {
+          if (key == 'url' || key == 'destDir') continue;
+          formData[key] = options[key];
+        }
+        request.post({url: options.url, formData: formData}, function (err, resp, body) {
+          if (err) {
+            gutil.log(red('err : ', err));
+          }
+          gutil.log(green(filePath, " => ", destPath, ", SUCCESS!"));
+        });
+      }
+      cb(null);
+    }
+  });
+};
